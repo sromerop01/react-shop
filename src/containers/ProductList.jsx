2ecf57d683ea361bc1587ebf2a8dfa9e5131941e
sromerop01 <sromerop2001@gmail.com>
2023-01-31 15:31:33 -0500

    [ADD] API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from 'components/ProductItem';
 import style from 'styles/ProductList.scss';
+import axios from 'axios';
+
+const API ='https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+    const [products, setProducts]= useState([]);
+
+    useEffect(async () => {
+        const response = await fetch(API);
+        const data = await response.json();
+        setProducts(data);
+    }, [])
+
     return (
         <section className="main-container">
         <div className="cards-container">
-            <ProductItem/>
+            {products.map( products => (
+                <ProductItem/>
+            ))}
         </div>
     </section>
     );
