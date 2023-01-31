2beeb806e8b478ad70e617f0aaeb3ab4076c2023
sromerop01 <sromerop2001@gmail.com>
2023-01-30 22:25:00 -0500

    [UPDATE] add pages to App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -14,10 +14,16 @@ const App = () => {
     <BrowserRouter>
     	<Layout>
         	<Routes>
-         		 <Route path="/" element={<Home />} />
-          		<Route path="/login" element={<Login />} />
-         		 <Route path="/password-recovery" element={<PasswordRecovery />} />
-          		<Route path="*" element={<NotFound />} />
+					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={PasswordRecovery} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
         	</Routes>
       	</Layout>
     </BrowserRouter>
