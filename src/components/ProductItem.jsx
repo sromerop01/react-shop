444a9703c347cc0c487e998b2b6babffd69a93ba
Santiago Romero <sromerop2001@gmail.com>
2023-02-13 09:35:03 -0500

    [UPDATE] Icon AddedToCart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import '../styles/ProductItem.scss';
-import Image from 'images/switch.jfif';
 import IconAddCart from 'icons/bt_add_to_cart.svg';
+import IconAddedCart from '../assets/Icons/bt_added_to_cart.svg';
 import AppContext from '../context/AppContext';
 
 
 const ProductItem = ({product}) => {
-    const { addToCart } = useContext(AppContext);
+    const { addToCart, removeFromCart, state } = useContext(AppContext);
+
+    const itsProductAdded = () => state.cart.some(item => item.id === product.id) ? true : false;
 
     const handleClick = item =>{
-        addToCart(item);
+        itsProductAdded() ? removeFromCart(item) : addToCart(item);
     }
 
     return ( 
@@ -21,7 +23,9 @@ const ProductItem = ({product}) => {
                     <p>{product.title}</p>
                 </div>
                 <figure onClick={()=> handleClick(product)}>
-                    <img src={IconAddCart} alt="cart"/>
+                    {itsProductAdded() 
+                    ? <img src={IconAddedCart} alt="AddedCart"/>
+                    : <img src={IconAddCart} alt="AddCart"/>}
                 </figure>
             </div>
         </div>
